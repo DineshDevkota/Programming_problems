d030f5be85bfd5da5a443a8346d61ceb247c6d93
Manpreet <manpreetlamsal@gmail.com>
2018-03-23 20:25:35 -0700

    Created new module to start code base. Hello seattle file added.

---
+++ practise/src/HelloSeattle.java
@@ -0,0 +1,14 @@
+public  class HelloSeattle {
+    public static void main(String args[]){
+        System.out.print("Hello there!");
+        //you there? waiting .....
+
+        // Git clone -> create the carbon copy of remote repository to your local machine
+        // new module .... this is just what a programmer does
+        // new files ..... then she fiddles around to make something work
+        // git add -> adds file to be tracked in local repository
+        // git commit -> this registers the file to local repository
+        // git push -> moves your code changes to the remote repository
+        // git pull -> to get the change list and files that other people in the project (The devils: pun intended) worked on.
+    }
+}
